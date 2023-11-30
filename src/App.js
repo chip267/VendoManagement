dfd67663b63ebe98b2994310f0b01236f102941e
Admin <tranthanhhien123bt@gmail.com>
2023-11-30 21:51:32 +0700

    dev: sidebar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Onboard from "./views/Onboard/onboard";
 import Login from "./views/Login/login";
 import Signup from "./views/Signup/Signup";
+import Sidebar from "./views/Sidebar/sidebar";
 export default function App() {
   return (
     <>
@@ -12,6 +13,7 @@ export default function App() {
           <Route path="/" exact={true} element={<Onboard />} />
           <Route path="/login" exact={true} element={<Login />} />
           <Route path="/signup" exact={true} element={<Signup />} />
+          <Route path="/sidebar" exact={true} element={<Sidebar />} />
         </Routes>
       </Router>
     </>
---
+++ src/views/Sidebar/sidebar.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { Route, Router, Routes } from "react-router-dom";
+import Dashboard from "../Dashboard/dashboard";
+import Products from "../Products/products";
+const tabNames = ["Dashboard", "Products"];
+
+const tabs = [<Dashboard />, <Products />];
+
+function Sidebar() {
+  const [index, SetIndex] = useState(0);
+  const tabList = tabNames.map((tab) => {
+    return (
+      <li>
+        <button onClick={() => SetIndex(tabNames.indexOf(tab))}>{tab}</button>
+      </li>
+    );
+  });
+  return (
+    <div>
+      <div>
+        <div className=" w-fit">
+          <p>Hello, Thanh Hien</p>
+          <p>See your account</p>
+          <ul className="my-10">{tabList}</ul>
+        </div>
+      </div>
+      <div>{tabs.at(index)}</div>
+    </div>
+  );
+}
+
+export default Sidebar;
