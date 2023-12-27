7401b623c79ea3207ecb4ccc0c2f3423012ba027
Shoru <bapcui596@gmail.com>
2023-12-28 06:58:38 +0700

    chore: route constants

---
+++ src/constants/route.js
@@ -0,0 +1,19 @@
+const ROUTE_CONSTANT = {
+    BASE: "/",
+    HOME: "/dashboard",
+    OPERATION:
+    {
+        PRODUCT: "/product",
+        CUSTOMER: "/customer",
+        SALE: "/sale",
+        EMPLOYEE: "/employee",
+    },
+    AUTH: {
+        SIGNUP: "/signup",
+        LOGIN: "/signin",
+    },
+    SIDEBAR: "/sidebar",
+    ABOUT: "/about"
+}
+
+export default ROUTE_CONSTANT;
