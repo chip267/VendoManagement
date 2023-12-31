610ae90d4e5bc2a85f30c50d5db861cb667e616b
Shoru <bapcui596@gmail.com>
2024-01-01 02:55:20 +0700

    feat(setting): simple user logout interface also added some simple user info display

--- src/views/Settings/settings.js
+++ src/views/Settings/settings.js
@@ -1,13 +1,61 @@
 import React from "react";
-
+import { UserApiController } from "../../api/user";
+import { useNavigate } from "react-router-dom";
+import { useUserContext } from "../../context/UserContext";
+import classNames from "classnames/bind";
+import styles from "./settings.module.scss";
+import { List } from "@mui/material";
+import { CircularProgress } from "@mui/material"
+const cx = classNames.bind(styles);
 function Settings() {
+  const { user, setUser } = useUserContext();
+  const Navigation = useNavigate();
+  const [isLoading, setIsLoading] = React.useState(false);
+  //Log out 
+  const logOut = async () => {
+    setIsLoading(true);
+    const response = await UserApiController.logout();
+    if (response.success) {
+      setUser(null);
+      Navigation("/signin");
+    }
+    else {
+      console.log("Log out failed");
+    }
+  }
+
+  const page = () => {
+    return (
+      <div className={cx("settings-container")}>
+        <h1 className={cx("settings-title")}>
+          Settings
+        </h1>
+        <div className={cx("settings-info")}>
+          <p className={cx("settings-info-username")}>
+            User name: {user ? user.username : ""}
+          </p>
+          <p className={cx("settings-info-role")}>
+            Role: {user ? user.role : ""}
+          </p>
+        </div>
+        <button className={cx("logout-btn")} onClick={logOut}>
+          Log out
+        </button>
+      </div>
+    );
+  };
   return (
-    <div className=" align-middle flex flex-col items-center pb-[200px]">
-      <h1 className=" text-center text-[31px] font-[Lexend] font-medium text-green_main mt-[75px] ">
-        Settings
-      </h1>
+    <div className={cx("settings")}>
+      {isLoading ? (
+        <div className="flex justify-center items-center py-4">
+          <CircularProgress />
+        </div>
+      ) : (
+        page()
+      )}
     </div>
   );
+
 }
 
 export default Settings;
