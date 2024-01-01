527689d06db7981027db4594b1e628138d09bf4c
Shoru <bapcui596@gmail.com>
2024-01-01 09:35:47 +0700

    feat(customer): customer api set up

---
+++ src/api/customer.js
@@ -0,0 +1,78 @@
+import apiInstance from ".";
+
+
+class CustomerController {
+    async getCustomers(
+        {
+            page = 1,
+            limit = 10 }
+    ) {
+        try {
+            const response = await apiInstance.get("/api/customers",
+                {
+                    withCredentials: true,
+                    params: { page, limit }
+                }
+            );
+            if (response.status === 200) {
+                return {
+                    success: true,
+                    data: response.data
+                };
+            }
+            else {
+                return {
+                    success: false,
+                    data: null
+                };
+            }
+        } catch (error) {
+            return error;
+        }
+    }
+    async getCustomer(id) {
+        try {
+            const response = await apiInstance.get("/api/customers/" + id, { withCredentials: true });
+            if (response.status === 200) {
+                return {
+                    success: true,
+                    data: response.data
+                };
+            }
+            else {
+                return {
+                    success: false,
+                    data: null
+                };
+            }
+        } catch (error) {
+            return error;
+        }
+    }
+    async addCustomer(customer) {
+        try {
+            const response = await apiInstance.post("/api/customers", customer, { withCredentials: true });
+            return response;
+        } catch (error) {
+            return error;
+        }
+    }
+    async updateCustomer(customer) {
+        try {
+            const response = await apiInstance.put("/api/customers/" + customer.id, customer, { withCredentials: true });
+            return response;
+        } catch (error) {
+            return error;
+        }
+    }
+    async deleteCustomer(id) {
+        try {
+            const response = await apiInstance.delete("/api/customers/" + id, { withCredentials: true });
+            return response;
+        } catch (error) {
+            return error;
+        }
+    }
+}
+const CustomerApiController = new CustomerController();
+export default CustomerApiController;
