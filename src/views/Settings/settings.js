dcefc1d1ae56c0814c69009d2ba39a447c2ac66c
Shoru <bapcui596@gmail.com>
2024-01-01 23:37:52 +0700

    chore(setting): additional information not styled

--- src/views/Settings/settings.js
+++ src/views/Settings/settings.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { UserApiController } from "../../api/user";
 import { useNavigate } from "react-router-dom";
 import { useUserContext } from "../../context/UserContext";
@@ -6,11 +6,14 @@ import classNames from "classnames/bind";
 import styles from "./settings.module.scss";
 import { List } from "@mui/material";
 import { CircularProgress } from "@mui/material"
+import EmployeeApiController from "../../api/employee";
 const cx = classNames.bind(styles);
+
 function Settings() {
   const { user, setUser } = useUserContext();
   const Navigation = useNavigate();
   const [isLoading, setIsLoading] = React.useState(false);
+  const [employeeInfo, setEmployeeInfo] = React.useState(null);
   //Log out 
   const logOut = async () => {
     setIsLoading(true);
@@ -23,7 +26,30 @@ function Settings() {
       console.log("Log out failed");
     }
   }
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    setIsLoading(true);
 
+    if (user) {
+      const response = await EmployeeApiController.getEmployee(user.employeeId);
+      if (response.success) {
+        if (user.role === "admin") {
+
+          let employee = response.data.employee;
+          employee.salary = "999.999.000 VND";
+          setEmployeeInfo(employee);
+        }
+        else {
+          let employee = response.data.employee;
+          employee.salary += " VND";
+          setEmployeeInfo(employee);
+        }
+      }
+    }
+    setIsLoading(false);
+  }
   const page = () => {
     return (
       <div className={cx("settings-container")}>
@@ -32,10 +58,16 @@ function Settings() {
         </h1>
         <div className={cx("settings-info")}>
           <p className={cx("settings-info-username")}>
-            User name: {user ? user.username : ""}
+            Username: {user ? user.username : ""}
           </p>
           <p className={cx("settings-info-role")}>
             Role: {user ? user.role : ""}
+          </p>
+          <p className={cx("settings-info-employee")}>
+            Name: {employeeInfo ? employeeInfo.name : ""}
+          </p>
+          <p className={cx("settings-info-salary")}>
+            Salary: {employeeInfo ? employeeInfo.salary : ""}
           </p>
         </div>
         <button className={cx("logout-btn")} onClick={logOut}>
