6d33811113aaf385fca552f78b7447f7020c7470
Shoru <bapcui596@gmail.com>
2024-01-03 01:04:17 +0700

    chore(sidebar): additional styling on icon also removed unecessary tabs

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,1 +1,5 @@
-export { default as IC_Home } from "./IC_Home.svg";
+export { default as IC_Home } from "./IC_Home_path.svg";
+export { default as IC_Customer } from "./IC_Customer_path.svg";
+export { default as IC_Product } from "./IC_Product_path.svg";
+export { default as IC_Sales } from "./IC_Sales_path.svg";
+export { default as IC_Setting } from "./IC_Settings_path.svg";
--- src/views/Sidebar/sidebar.js
+++ src/views/Sidebar/sidebar.js
@@ -3,16 +3,24 @@ import Dashboard from "../Dashboard/dashboard";
 import Products from "../Products/products";
 import Customers from "../Customers/customers";
 import { IMG_Logo } from "../../assets/images";
-import { IC_Home } from "../../assets/icons";
+import { IC_Home, IC_Customer, IC_Product, IC_Sales, IC_Setting } from "../../assets/icons";
 import Sales from "../Sales/sales";
 import Settings from "../Settings/settings";
-
 const tabNames = [
   "Dashboard",
   "Products",
   "Customers",
   "Sales",
   "Settings",
+];
+
+
+const tabIcons = [
+  IC_Home,
+  IC_Product,
+  IC_Customer,
+  IC_Sales,
+  IC_Setting,
 ];
 const tabs = [
   <Dashboard />,
@@ -21,20 +29,37 @@ const tabs = [
   <Sales />,
   <Settings />,
 ];
+
 function Sidebar() {
   const [index, setIndex] = useState(0);
-
+  const styleSelected = {
+    backgroundColor: "#EBFAED",
+    color: "#2C7A51",
+    borderRadius: "10px",
+    padding: "10px",
+    //
+  };
+  const normalStyle = {
+    borderRadius: "10px",
+    padding: "10px",
+  }
+  const iconSelected = {
+    filter: "invert(40%) sepia(100%) saturate(10000%) hue-rotate(100deg) brightness(100%) contrast(100%)"
+  }
   const tabList = tabNames.map((tab) => {
     const key = tabNames.indexOf(tab);
     return (
       <li key={key} className="flex mt-[25px] ml-[24px]">
-        <img className="w-[20px]" src={IC_Home} alt="Mô tả hình ảnh" />
-        <button
-          onClick={() => setIndex(tabNames.indexOf(tab))}
-          className="text-[11px] font-[Lexend] font-medium text-sidebar_txt ml-[16px]"
-        >
-          {tab}
-        </button>
+        <div className="flex items-center" style={index === key ? styleSelected : normalStyle}>
+          <img className="w-[20px] h-[20px] mr-[16px]" style={index === key ? iconSelected : {}}
+            src={tabIcons[key]} alt="Mô tả hình ảnh" />
+          <button style={index === key ? { color: "#2C7A51" } : {}}
+            onClick={() => setIndex(tabNames.indexOf(tab))}
+            className="text-[11px] font-[Lexend] font-medium text-sidebar_txt ml-[16px]"
+          >
+            {tab}
+          </button>
+        </div>
       </li>
     );
   });
