dc56d3e19262e74ad38e02d8f1762d982294ef1e
Shoru <bapcui596@gmail.com>
2024-01-08 15:12:49 +0700

    feat(employee): additional filter

--- src/api/employee.js
+++ src/api/employee.js
@@ -1,12 +1,28 @@
 import apiInstance from ".";
 
-
-
 class EmployeeContorller {
     currentEmployee = null;
-    async getEmployees({ limit = 10, page = 1, sortBy = "name", sort = "asc", search = "" } = {}) {
+    async getEmployees(
+        {
+            limit = 10,
+            page = 1,
+            name = null,
+            phoneNumber = null,
+        }) {
         try {
-            const response = await apiInstance.get("/api/employees", { withCredentials: true });
+            console.log("name is " + name);
+            const response = await apiInstance.get("/api/employees", {
+                params: {
+                    limit: limit,
+                    page: page,
+                    name: name,
+                    phoneNumber: phoneNumber,
+                },
+                withCredentials: true
+            });
+            //Convert from json
+
+
             if (response.status === 200) {
                 return {
                     success: true,
@@ -20,7 +36,11 @@ class EmployeeContorller {
                 };
             }
         } catch (error) {
-            return error;
+            console.log("API error: " + error);
+            return {
+                success: false,
+                data: null
+            };
         }
     }
     async getEmployee(id) {
@@ -39,7 +59,13 @@ class EmployeeContorller {
                 };
             }
         } catch (error) {
-            return error;
+            console.log("API error: " + error);
+
+            return {
+                success: false,
+                data: null
+            };
+
         }
     }
     //set current employee
@@ -49,28 +75,91 @@ class EmployeeContorller {
     getCurrentEmployee() {
         return this.currentEmployee;
     }
+    async changePassword(id, password) {
+        try {
+            const response = await apiInstance.post("/api/change-password/" + id, {
+                password: password
+            }, { withCredentials: true });
+            return {
+                success: true,
+                data: response.data
+            };
+        }
+        catch (error) {
+            console.log("API error: " + error);
+            return {
+                success: false,
+                data: null
+            };
+        }
+    }
     async addEmployee(employee) {
         try {
             const response = await apiInstance.post("/api/employees", employee, { withCredentials: true });
-            return response;
+            return {
+                success: true,
+                data: response.data
+            };
+
         } catch (error) {
-            return error;
+            console.log("API error: " + error);
+            if (error.response.status === 400) {
+                return {
+                    success: false,
+                    data: "400"
+                };
+            }
+
+            return {
+                success: false,
+                data: null
+            };
+
         }
     }
-    async updateEmployee(employee) {
+    async updateEmployee({
+        _id,
+        phoneNumber = null,
+        name = null,
+        address = null,
+        salary = null,
+        position = null,
+    }) {
         try {
-            const response = await apiInstance.put("/api/employees/" + employee.id, employee, { withCredentials: true });
-            return response;
+            const response = await apiInstance.patch("/api/employees/" + _id, {
+                phoneNumber: phoneNumber,
+                name: name,
+                address: address,
+                salary: salary,
+                position: position,
+            }, { withCredentials: true });
+            return {
+                success: true,
+                data: response.data
+            };
+
         } catch (error) {
-            return error;
+            console.log("API error: " + error);
+            return {
+                success: false,
+                data: null
+            };
+
         }
     }
     async deleteEmployee(id) {
         try {
             const response = await apiInstance.delete("/api/employees/" + id, { withCredentials: true });
-            return response;
+            return {
+                success: true,
+                data: response.data
+            };
         } catch (error) {
-            return error;
+            console.log("API error: " + error);
+            return {
+                success: false,
+                data: null
+            };
         }
     }
 }
